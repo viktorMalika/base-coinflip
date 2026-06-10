496fb9470599df049dd968dd857c76a06b008f09
viktorMalika <viktorMalika@users.noreply.github.com>
2026-06-10 09:00:39 +0000

    style: prettier formatting

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+# Configuration reference
 
 import {Script, console} from "forge-std/Script.sol";
 import {CoinFlip} from "../src/CoinFlip.sol";
